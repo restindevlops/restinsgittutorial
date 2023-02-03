1c7c807c2277ede433f78853a69941608ba5f572
restindevlops <restincraji@gmail.com>
2023-02-03 23:18:06 +0530

    getelementsbyclassname

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,17 +1,28 @@
 //document.title= "Page title Changed";
 //console.log(document.title);
-let x= document.getElementById("header-title");
-x.style.borderBottom="solid 4px #000";
-let y=document.getElementsByClassName("title1");
+//let x= document.getElementById("items");
+//console.log(x);
+//x.style.borderBottom="solid 4px #000";
+//let y=document.getElementsByClassName("title1");
 ///console.log(y);
 //y.style.fontWeight ='bold';
 //y.style.fontcolor='yellow';
-for(let i=0;i<y.length;i++)
+/*for(let i=0;i<y.length;i++)
 {
     y[i].style.fontWeight ='bold';
     y[i].style.color='yellow';
     y[i].style.backgroundColor='black';
+}*/
+let items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight ='bold';
 }
+    
+
+
 
 
 
