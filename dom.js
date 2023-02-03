1b6bf8c143e03f5182a4ac0ed4d20f2f400c9908
restindevlops <124300271+restindevlops@users.noreply.github.com>
2023-02-04 00:14:40 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -21,14 +21,22 @@ for(let i=0;i<items.length;i++)
     items[i].style.fontWeight ='bold';
 }*/
     
-let li=document.getElementsByTagName('li');
+/*let li=document.getElementsByTagName('li');
 console.log(li);
 li[2].style.backgroundColor='green';
 for(let i=0;i<li.length;i++)
 {
     li[i].style.fontWeight ='bold';
     li[i].style.backgroundColor='yellow';
-}
+}*/
+let seconditem=document.querySelector
+('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='green';
+
+let thirditem=document.querySelector
+('.list-group-item:nth-child(3)');
+//console.log(thirditem);
+thirditem.style="display:none";
 
 
 
